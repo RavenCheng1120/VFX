c80597579b87eef58da64350bb77ac0584b32591
RavenCheng1120 <ravencheng66@gmail.com>
2021-03-25 22:22:41 +0800

    fix read order & threshold

--- HDRI.py
+++ HDRI.py
@@ -6,19 +6,25 @@ def readImages(folder):
 	imageFormat = ['png', 'jpg']
 	files = []
 	[files.extend(glob.glob(folder + '*.' + e)) for e in imageFormat]
-	imageList = [cv2.imread(file) for file in files]
+	imageList = [cv2.imread(file) for file in sorted(files)]
 	grayscaleList = turnGrayscale(imageList)
 	# print(len(imageList))
 	return imageList, grayscaleList
 
+def showImage(img):
+	cv2.imshow('image', img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	return
 
 def turnGrayscale(imageList):
 	grayscaleList = []
+	count = 0
 	for img in imageList:
 		grayscaleList.append(np.dot(img[...,:3], [19/256, 183/256, 54/256]).astype(np.uint8))
-	# cv2.imshow('grayscaleImage', grayscaleList[0])
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
+		# grayscaleList.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+		# cv2.imwrite('./grayscale/'+str(count)+'.png', grayscaleList[count])
+		count += 1
 	return grayscaleList
 
 
@@ -27,6 +33,8 @@ def binaryThreshold(imageList):
 	count = 0
 	for img in imageList:
 		threshold = np.median(img)
+		if threshold < 20.0:
+			threshold = 20.0
 		binaryImage = np.zeros(imageList[0].shape, np.uint8)
 		for row in range(len(img)):
 			for column in range(len(img[0])):
@@ -36,12 +44,25 @@ def binaryThreshold(imageList):
 					binaryImage[row][column] = 0
 		binaryImageList.append(binaryImage)
 		print("Binary image " + str(count) + " processing...")
+		print(threshold)
+		# cv2.imwrite('./binary/'+str(count)+'.png', binaryImage)
 		count += 1
 	return binaryImageList
 
+
+def excludeMask(imageList):
+	maskImageList = []
+	for img in imageList:
+		mask_img = cv2.inRange(img, np.median(img) - 4, np.median(img) + 4)
+		maskImageList.append(mask_img)
+	return maskImageList
+
+
+# def MedianThresholdBitmap():
 
 
 
 if __name__ == '__main__':
 	sourceImages, grayImages = readImages('Memorial_SourceImages/')
 	binaryImages = binaryThreshold(grayImages)
+	maskImages = excludeMask(grayImages)
